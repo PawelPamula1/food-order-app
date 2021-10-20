4baa701a74a44f2466cecfa7251911659c9cd602
PawelPamula1 <pawelpamula003@gmail.com>
2021-10-20 23:08:50 +0200

    adding more logic funcionality to the cart component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,10 +34,11 @@ function AvailableMeals() {
   const mealsList = DUMMY_MEALS.map((meal) => {
     return (
       <MealItem
+        key={meal.id}
+        id={meal.id}
         name={meal.name}
         description={meal.description}
         price={meal.price}
-        key={meal.id}
       />
     );
   });
