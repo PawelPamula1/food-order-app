157f29bbd04e8dc7716af04a34ace1b370e65b0a
PawelPamula1 <pawelpamula003@gmail.com>
2021-10-20 16:44:40 +0200

    changing cartIcon directory

--- src/Layout/HeaderCartButton.js
+++ src/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CartIcon from '../Cart/CartIcon';
+import CartIcon from '../components/Meals/Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 function HeaderCartButton(props) {
