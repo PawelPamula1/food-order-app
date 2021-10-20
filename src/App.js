15da45e648189ddcde6b4eebe701b158c93e8c70
PawelPamula1 <pawelpamula003@gmail.com>
2021-10-20 15:26:25 +0200

    adding Modal Component through the portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Meals/Cart/Cart';
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
